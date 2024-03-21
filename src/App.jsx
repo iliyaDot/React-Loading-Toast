1fba25b105e5ac29eb6ff3ee301562e6175fbf1c
iliya <iliyahm@yahoo.com>
2024-03-21 21:20:42 +0000

    added fetch json to the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Loading from './Components/Loading';
 import Toast from './Components/Toast/Toast';
 
@@ -9,10 +9,22 @@ import Toast from './Components/Toast/Toast';
 export default function App() {
 
 const [toast ,  setToast]=useState({type: 'info', message:'heloo'}) // for me : bara inke information dar yekjaie zakhire konim az usestate estefade kardim va ghesmat pain
+const [title ,setTitle]= useState('')
+useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then (post =>setTitle(post.title))
+})
 
   return (
-    <div>
-<Loading />
+//     //to in version dare bug mikhure age laoding hamntori bashe matn json neshon nmide pas bayad barash shart bashe //
+// {/* <Loading /> */} 
+<div>
+<div>
+<h1>{title}</h1>
+
+</div>
+
 <Toast type={toast.type} message={toast.message}/>
     </div>
   )
